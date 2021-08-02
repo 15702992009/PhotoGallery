49bb58eed05e9416bb10b358d6a17bcb22b300b9
JiaboWang <jumbola@163.com>
2021-08-02 23:47:32 +0800

    20201-08-02

--- app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.java
@@ -9,7 +9,7 @@ public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return null;
+        return new PhotoGalleryFragment();
     }
 
 
